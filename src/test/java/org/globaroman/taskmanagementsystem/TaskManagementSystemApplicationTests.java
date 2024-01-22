f5505d06a1c63d0e632bc309c8803eda215b7ee0
Roman Globa <globaroman@gmail.com>
2024-01-22 21:16:02 +0200

    Remove ignored files

---
+++ src/test/java/org/globaroman/taskmanagementsystem/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.globaroman.taskmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
